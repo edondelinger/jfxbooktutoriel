efea793a465c912098ad9ddaf96f7199ef6affe3
eric <edondelinger@gmail.com>
2022-09-21 17:33:01 +0200

    Premier commit. Fin du Tuto

--- src/main/java/com/example/bookapp/AccueilController.java
+++ src/main/java/com/example/bookapp/AccueilController.java
@@ -1,4 +1,98 @@
 package com.example.bookapp;
 
-public class Accueil {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AccueilController  implements Initializable {
+
+    @FXML
+    private Button BtnAdd;
+
+    @FXML
+    private Button BtnDelete;
+
+    @FXML
+    private HBox HBox4Btns;
+
+    @FXML
+    private Label LblAddText;
+
+    @FXML
+    private ListView<Book> listBoxMain;
+
+    @FXML
+    private Label TitleLbl;
+
+    @FXML
+    private VBox VBoxMain;
+
+    @FXML
+    private TextField txtAddItem;
+
+    final ObservableList<Book> listItems = FXCollections.observableArrayList();
+
+    // Add event handlers
+    @FXML
+    private void addAction(ActionEvent action){
+        //listItems.add(txtAddItem.getText());
+        System.out.println("Add Button Pushed");
+    }
+
+
+    @FXML
+    private void deleteAction(ActionEvent action){
+        int selectedItem = listBoxMain.getSelectionModel().getSelectedIndex();
+        listItems.remove(selectedItem);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        Book book1 = new Book(1L, "J01", "Java IO Tutorial");
+        Book book2 = new Book(2L, "J02", "Java Enums Tutorial");
+        Book book3 = new Book(2L, "C01", "C# Tutorial for Beginners");
+
+        listItems.add(book1);
+        listItems.add(book2);
+        listItems.add(book3);
+
+        listBoxMain.setItems(listItems);
+
+        // Disable buttons to start
+        BtnAdd.setDisable(true);
+        BtnDelete.setDisable(true);
+
+        // Add a ChangeListener to TextField to look for change in focus
+        txtAddItem.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if(txtAddItem.isFocused()){
+                    BtnAdd.setDisable(false);
+                }
+            }
+        });
+
+        // Add a ChangeListener to ListView to look for change in focus
+        listBoxMain.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if(listBoxMain.isFocused()){
+                    BtnDelete.setDisable(false);
+                }
+            }
+        });
+
+    }
 }
--- src/main/java/com/example/bookapp/HelloController.java
+++ src/main/java/com/example/bookapp/HelloController.java
@@ -1,14 +1,47 @@
 package com.example.bookapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField monID;
+    @FXML
+    private TextField monMDP;
+    @FXML
+    protected void onHelloButtonClick() throws IOException {
         welcomeText.setText("Welcome to JavaFX Application!");
+        String id, mdp;
+
+        id = monID.getText();
+        mdp = monMDP.getText();
+
+        if (id.equals("admin") && mdp.equals("admin")) {
+
+            Stage newWindow = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("accueil.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+            newWindow.setScene(scene);
+            // Specifies the modality for new window.
+            newWindow.initModality(Modality.APPLICATION_MODAL);
+            newWindow.show();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Erreur de connexion");
+            alert.setHeaderText("Connexion");
+            alert.setContentText("utilisateur non reconnu. Réessayez !");
+            alert.showAndWait();
+        }
+
     }
 }
